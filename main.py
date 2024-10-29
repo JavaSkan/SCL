afadf6fda863f5b60cb75367e5a8e74280390f1e
JavaSkan <skan13112006@gmail.com>
2024-10-29 17:38:18 +0100

    ability to run a file with scl in the command prompt, shell etc.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from runtime import execution
+import os
+import sys
 import threading as th
-
 from runtime.execution import Executor
 
 opening_message = """
@@ -12,13 +12,21 @@ All rights reserved©
 """
 
 def main():
-    print(opening_message)
-    while True:
-        with Executor() as mainExecutor:
-            script = input("?> ")
-            exth = th.Thread(target=mainExecutor.execute,args=[script])
-            exth.start()
-            exth.join()
+    if len(sys.argv) >= 2:
+        if os.path.exists(sclpath := (sys.argv[1])):
+            if not sclpath.endswith(".scl"):
+                print(f"wrong extension, should end with '.scl'", file=sys.stderr)
+            Executor().execute(f'exec "{sclpath}"')
+        else:
+            print(f"{sclpath} doest not exist",file=sys.stderr)
+    else:
+        print(opening_message)
+        while True:
+            with Executor() as mainExecutor:
+                script = input("?> ")
+                exth = th.Thread(target=mainExecutor.execute,args=[script])
+                exth.start()
+                exth.join()
 
 if __name__ == '__main__':
     main()
